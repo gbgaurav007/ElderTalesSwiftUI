de29be50afb279883b889c2bfd4ed776c83d3ce5
Gaurav Bansal <gbgauravbansal007@gmail.com>
2024-12-04 15:36:21 +0530

    Create ElderTales

---
+++ ElderTales/ElderTales/ElderTalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElderTalesApp.swift
+//  ElderTales
+//
+//  Created by Gaurav Bansal on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ElderTalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElderTales/ElderTalesTests/ElderTalesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ElderTalesTests.swift
+//  ElderTalesTests
+//
+//  Created by Gaurav Bansal on 04/12/24.
+//
+
+import Testing
+
+struct ElderTalesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
